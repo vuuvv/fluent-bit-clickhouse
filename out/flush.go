0adc6f6b45c9086e98d046eacc523a947a3d3700
vuuvv <vuuvv@qq.com>
2023-08-18 15:41:15 +0800

    fix: add log

--- out/flush.go
+++ out/flush.go
@@ -133,11 +133,13 @@ func (this *ClickHouseClient) Flush(dec *output.FLBDecoder) int {
 						reqLog.BaseLog = log.BaseLog
 					}
 					this.reqBuffer = append(this.reqBuffer, reqLog)
+					klog.Info("req log ", log.Log, " ", len(this.reqBuffer))
 				case "sql":
 					sqlLog := SqlLog{}
 					sqlLog.BaseLog = log.BaseLog
 					ParseSqlLog(log.Log, &sqlLog)
 					this.sqlBuffer = append(this.sqlBuffer, sqlLog)
+					klog.Info("sql log ", log.Log, " ", len(this.sqlBuffer))
 				}
 			}
 			// 如果有错误就不处理
